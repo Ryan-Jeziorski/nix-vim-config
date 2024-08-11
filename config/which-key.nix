0cdec7826a05fb7ccf759461e132acd1d3d47309
ryan@roshar <rjeziorski@protonmail.com>
2024-08-11 17:31:23 -0500

    Switch to classic preset

--- config/which-key.nix
+++ config/which-key.nix
@@ -1,66 +1,6 @@
 {
   plugins.which-key.enable = true;
   plugins.which-key.settings = {
-    delay = 200;
-    expand = 1;
-    notify = false;
-    preset = false;
-    replace = {
-      desc = [
-        [
-          "<space>"
-          "SPACE"
-        ]
-        [
-          "<leader>"
-          "SPACE"
-        ]
-        [
-          "<[cC][rR]>"
-          "RETURN"
-        ]
-        [
-          "<[tT][aA][bB]>"
-          "TAB"
-        ]
-        [
-          "<[bB][sS]>"
-          "BACKSPACE"
-        ]
-      ];
-    };
-    spec = [
-      {
-        __unkeyed-1 = "<leader>b";
-        group = "󰓩 Buffers";
-      }
-      {
-        __unkeyed-1 = "<leader>bs";
-        group = "󰒺 Sort";
-      }
-      {
-        __unkeyed-1 = "<leader>g";
-        group = "󰊢 Git";
-      }
-      {
-        __unkeyed-1 = "<leader>f";
-        group = " Find";
-      }
-      {
-        __unkeyed-1 = "<leader>r";
-        group = " Refactor";
-      }
-      {
-        __unkeyed-1 = "<leader>t";
-        group = " Terminal";
-      }
-      {
-        __unkeyed-1 = "<leader>u";
-        group = " UI/UX";
-      }
-    ];
-    win = {
-      border = "single";
-    };
+    preset = "classic";
   };
 }
