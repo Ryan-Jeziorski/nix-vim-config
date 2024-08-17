1df9ab1696e656743adf3dcd9faf0ea3ca00360d
Ryan <rjeziorski@outlook.com>
2024-08-16 22:46:17 -0500

    Update confg, trying to get pop out windows working

--- config/cmp.nix
+++ config/cmp.nix
@@ -1,9 +1,12 @@
 {
   plugins = {
+    luasnip.enable = true;
     cmp-buffer = { enable = true; };
     cmp-nvim-lsp = {enable = true; };
     cmp-path = {enable = true; };
     cmp-nerdfont = {enable = true; };
+    cmp_luasnip = { enable = true; };
+
 
     cmp = {
       enable = true;
@@ -12,11 +15,11 @@
         experimental = { ghost_text = true; };
         sources = [
           { name = "nvim_lsp"; }
+          { name = "luasnip"; }
+          { name = "path"; }
           { 
             name = "buffer";
-            options.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
-          }
-          { name = "path"; }
+            option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";}
         ];
 
         formatting = {
@@ -109,6 +112,32 @@
                   fallback()
                 elseif cmp.visible() then
                   cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
+                else
+                  fallback()
+                end
+              end
+            '';
+             "<Down>" =
+            # lua
+            ''
+              function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif require("luasnip").expand_or_jumpable() then
+                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+                else
+                  fallback()
+                end
+              end
+            '';
+          "<Up>" =
+            # lua
+            ''
+              function(fallback)
+                if cmp.visible() then
+                  cmp.select_prev_item()
+                elseif require("luasnip").jumpable(-1) then
+                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
                 else
                   fallback()
                 end
