b302a7211d5f0ccec624bbf922631fc4532a1c33
Ryan <rjeziorski@outlook.com>
2024-08-18 14:24:06 -0500

    Make escape :noh clear search

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -2,10 +2,18 @@
   globals.mapleader = " ";
   keymaps = [
     {
+      key = "<esc>";
+      action = ":noh<CR>";
+      options.desc = "Clear search highlighting with esc";
+    }
+
+    # nvim-tree file explorer
+    {
       key = "<leader>f";
       action = "<CMD>NvimTreeToggle<CR>";
       options.desc = "Toggle NvimTree";
     }
+
     # Rust
     {
       mode = "n";
