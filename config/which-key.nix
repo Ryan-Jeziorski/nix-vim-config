95821ea2181d2853550c2235577b5408b050fabe
Ryan <rjeziorski@protonmail.com>
2025-05-07 17:35:17 -0500

    Switch presets up

--- config/which-key.nix
+++ config/which-key.nix
@@ -1,6 +1,15 @@
 {
   plugins.which-key.enable = true;
-  plugins.which-key.settings = {
-    preset = "modern";
+
+  #plugins.which-key.settings = {
+    #preset = "modern";
+  plugins.which-key.settings.plugins.presets = {
+    operators = true; # -- adds help for operators like d, y, ...
+    motions = true; #, -- adds help for motions
+    text_objects = true; #, -- help for text objects triggered after entering an operator
+    windows = true; #, -- default bindings on <c-w>
+    nav = true; #, -- misc bindings to work with windows
+    z = true; #, -- bindings for folds, spelling and others prefixed with z
+    g = true; #, -- bindings for prefixed with g
   };
 }
